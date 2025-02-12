50a5daf4161427cc2cd36958c7beea65622e6a2d
Aleksei Erofeev <54234310+Aleks3y1@users.noreply.github.com>
2025-02-12 23:40:17 +0500

    Исправляет tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,15 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import handlebars from "vite-plugin-handlebars";
-import NodePolyfills from "vite-plugin-node-polyfills";
 
 export default defineConfig({
     server: {
         port: 3000,
     },
     plugins: [
-        NodePolyfills(),
         handlebars({
             partialDirectory: "public/templates/partials",
         }),
     ],
-    resolve: {
-        alias: {
-            crypto: "crypto-browserify",
-        },
-    },
-    define: {
-        global: "window",
-        "process.env": {},
-    },
     assetsInclude: ["**/*.hbs"],
     build: {
         outDir: "dist",
